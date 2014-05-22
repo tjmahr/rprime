0c08d9c6856b229fd9e97ec17a37192907588f21
TJ Mahr <mahr@wisc.edu>
2014-05-22 10:22:50 -0500

    more development

--- R/extract.R
+++ R/extract.R
@@ -1,7 +1,64 @@
+#' @export
+clean_up_list <- function(eprime_list) {
+  level_name <- eprime_list[["Running"]]
+  if (!is.null(level_name)) {
+    # Remove the level name from the names in the list
+    eprime_list <- set_which_name(eprime_list, level_name, "Eprime.LevelIndex")
+    eprime_list[["Eprime.LevelIndex"]] <- paste0(level_name, "_", eprime_list[["Eprime.LevelIndex"]])
+    names(eprime_list) <- str_replace(names(eprime_list), paste0(level_name, "\\."), "")
+  }
+  eprime_list
+}
+
+which_name <- function(list, name) {
+  which(names(list) == name)
+}
+
+set_which_name <- function(list, name, new_name) {
+  index <- which_name(list, name)
+  names(list)[index] <- new_name
+  list
+}
+
+
+# Convert a vector of colon-separated values into a list of named elements.
+#
+#   > example <- c("Item: 1", "OtherItem: Stuff", "Last Item: Whatever")
+#   > Listify(example)
+#   $Item
+#   [1] "1"
+#
+#   $OtherItem
+#   [1] "Stuff"
+#
+#   $`Last Item`
+#   [1] "Whatever"
+#
+
+#' @export
+listify <- function(colon_sep_values) {
+  # Infer level of nesting by counting tabs
+  tab_count <- str_count(colon_sep_values[1], "\\t")
+  level <- tab_count + 1
+  colon_sep_values <- c(colon_sep_values, paste0("Eprime.Level: ", level))
+
+  # Clean up the lines and listify
+  colon_sep_values <- str_trim(colon_sep_values)
+  colon_sep_values <- Filter(Negate(is_bracket), colon_sep_values)
+
+  splits <- str_split_fixed(colon_sep_values, pattern = ": ", 2)
+  structure(as.list(splits[, 2]), names = splits[, 1])
+}
+
+
+
+
+
+#' @export
 extract_frames <- function(eprime_log) {
   # Find all the texts between LogFrame boundaries
-  starts <- str_detect_which(eprime_log, patterns$bracket_start)
-  ends <- str_detect_which(eprime_log, patterns$bracket_end)
+  starts <- str_which(eprime_log, patterns$bracket_start)
+  ends <- str_which(eprime_log, patterns$bracket_end)
   ranges <- make_ranges(starts, ends, eprime_log)
 
   pull_lines <- function(lines) eprime_log[lines]
@@ -18,15 +75,16 @@ make_ranges <- function(starts, ends, eprime_log) {
   ends <- ends[seq_len(min_chunks)]
 
   # Warn if there is an incomplete frame
-  bad_line <- max(setdiff(old_starts, starts))
+  bad_line <- setdiff(old_starts, starts)
   if (!length_zero(bad_line)) {
-    lines_after <- length(eprime_log) - bad_line
+    last_bad_line <- max(bad_line)
+    lines_after <- length(eprime_log) - last_bad_line
 
     # Give a preview of the incomplete chunk in the warning
     if (lines_after < 10) {
-      sample_range <- seq(bad_line, length(eprime_log))
+      sample_range <- seq(last_bad_line, length(eprime_log))
     } else {
-      sample_range <- seq(bad_line, bad_line + 10)
+      sample_range <- seq(last_bad_line, last_bad_line + 10)
     }
 
     warning_header <- paste0("Incomplete Log Frame found on line ", bad_line)
@@ -42,6 +100,10 @@ make_ranges <- function(starts, ends, eprime_log) {
   mapply(seq, starts, ends)
 }
 
-str_detect_which <- function(string, pattern) which(str_detect(string, pattern))
+#' @export
+str_which <- function(string, pattern) {
+  which(str_detect(string, pattern))
+}
+
 length_zero <- function(x) length(x) == 0
 length_one <- function(x) length(x) == 1
---
+++ R/primer.R
@@ -0,0 +1,6 @@
+#' Functions for dealing with Eprime txt files
+#'
+#' @name primer
+#' @docType package
+#' @import stringr
+NULL
