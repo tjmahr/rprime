7aaf0c83ace43d4fe23cd40712fcff340c5d3132
unknown <mahr@wisc.edu>
2014-05-22 13:38:19 -0500

    document list functions

--- R/utils.R
+++ R/utils.R
@@ -32,16 +32,23 @@
 #' @param keys a character vector of names used to simplify the list
 #' @param xs a list
 #' @param xss a list of lists
+#' @return \code{pluck} returns an unnamed value and \code{pluck_apply} returns
+#'   a list of unnamed values. \code{pick} and \code{omit} return a simplified
+#'   version of the original list. \code{pick_apply} and \code{omit_apply}
+#'   return a list of simplified lists.
 #'
-#' \itemize{
-#'   \item \code{pluck}: Pluck a named value from a list
-#'   \item \code{pick}: Simplify a list by picking out whitelisted names
-#'   \item \code{omit}: Simplify a list by omitting blacklisted names.
-#' }
+#' @details \itemize{ \item \code{pluck}: Pluck a named value from a list \item
+#' \code{pick}: Simplify a list by picking out whitelisted names \item
+#' \code{omit}: Simplify a list by omitting blacklisted names. }
+#'
+#' The simple versions of \code{pluck}, \code{pick}, and \code{omit} are curried
+#' functions, meaning that they return a function which can be applied to a
+#' list. See the syntax in the usage section.
 NULL
 
 
 #' @rdname list_functions
+#' @usage pluck(key)(xs)
 #' @export
 pluck <- function(key) {
   function(xs) xs[[key]]
@@ -55,6 +62,7 @@ pluck_apply <- function(xss, key) {
 
 
 #' @rdname list_functions
+#' @usage pick(keys)(xs)
 #' @export
 pick <- function(keys) {
   function(xs) xs[is.element(names(xs), keys)]
@@ -67,6 +75,7 @@ pick_apply <- function(xss, keys) {
 }
 
 #' @rdname list_functions
+#' @usage omit(keys)(xs)
 #' @export
 omit <- function(keys) {
   function(xs) xs[!is.element(names(xs), keys)]
