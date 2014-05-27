cf0580d946083cddb5453dbaa0d36b63f812550e
unknown <mahr@wisc.edu>
2014-05-27 11:25:09 -0500

    tests for file-loading function

---
+++ inst/tests/test-load.R
@@ -0,0 +1,46 @@
+require("stringr")
+
+# Test files
+bad_encoding <- "data/Blending_001L00XS4.txt"
+crashed_experiment <- "data/MP_Block1_001P00XA1.txt"
+no_footer <- "data/Coartic_Block1_001P00XS1.txt"
+
+is_header <- function(x) x == "*** Header Start ***"
+first_line <- function(xs) xs[1]
+count_blanks <- function(xs) sum(str_detect(xs, "^$"))
+
+
+context("load file with unexpected encoding")
+
+test_that("naive file-loading fails", {
+  expect_warning(readLines(bad_encoding, encoding = "UCS-2LE"),
+                 regexp = "embedded nul")
+  expect_more_than(count_blanks(readLines(bad_encoding, skipNul = TRUE)), 1)
+})
+
+test_that("read_eprime works", {
+  bad_encoding_lines <- read_eprime(bad_encoding)
+  expect_true(is_header(first_line(bad_encoding_lines)))
+  expect_less_than(count_blanks(bad_encoding_lines), 1)
+})
+
+
+context("load file from a crashed experiment")
+
+test_that("naive file-loading fails", {
+  expect_warning(readLines(crashed_experiment, skipNul = TRUE),
+                 regexp = "incomplete final line")
+})
+
+test_that("read_eprime works", {
+  crashed_experiment_lines <- read_eprime(bad_encoding)
+  expect_true(is_header(first_line(crashed_experiment_lines)))
+  expect_less_than(count_blanks(crashed_experiment_lines), 1)
+})
+
+
+
+
+
+
+
