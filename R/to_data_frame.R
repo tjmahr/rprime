55aa10117534dab734f0344cc13017acac15caf4
unknown <mahr@wisc.edu>
2015-05-22 13:28:33 -0500

    remove as.data.frame.EprimeFrame

--- R/to_data_frame.R
+++ R/to_data_frame.R
@@ -8,33 +8,27 @@
 #'
 #' @param x an EprimeFrame object, or a FrameList object (a list of
 #'   EprimeFrames)
-#' @param ... other parameters passed onto \code{as.data.frame}
 #' @return all of the EprimeFrames combined into a single data frame.
 #' @export
 #' @seealso \link{rbind.fill}
-to_data_frame <- function(x, ...) {
+to_data_frame <- function(x) {
   UseMethod("to_data_frame")
 }
 
 #' @export
-to_data_frame.default <- function(x, ...) {
-  data_frames <- lapply(x, as.data.frame, stringsAsFactors = FALSE, ...)
+to_data_frame.default <- function(x) {
+  data_frames <- lapply(x, as.data.frame.list, stringsAsFactors = FALSE)
   rbind.fill(data_frames)
 }
 
 #' @export
-to_data_frame.FrameList <- function(x, ...) {
+to_data_frame.FrameList <- function(x) {
   data_frames <- lapply(x, to_data_frame)
   rbind.fill(data_frames)
 }
 
 #' @export
-to_data_frame.EprimeFrame <- function(x, ...) {
-  as.data.frame.list(x, stringsAsFactors = FALSE, ...)
+to_data_frame.EprimeFrame <- function(x) {
+  as.data.frame.list(x, stringsAsFactors = FALSE)
 }
 
-#' @export
-as.data.frame.FrameList <- to_data_frame.FrameList
-
-#' @export
-as.data.frame.EprimeFrame <- to_data_frame.EprimeFrame
