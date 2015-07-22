6722a8c036d9949a7854225d8dbc130246ecb19c
TJ Mahr <mahr@wisc.edu>
2015-07-22 16:27:17 -0500

    Static docs initialized

--- R/rprime.R
+++
@@ -1,9 +0,0 @@
-#' Functions for dealing with Eprime txt files
-#'
-#' @name rprime
-#' @docType package
-#' @import stringr
-#' @import plyr
-#' @import assertthat
-#' @importFrom utils str tail head
-NULL
